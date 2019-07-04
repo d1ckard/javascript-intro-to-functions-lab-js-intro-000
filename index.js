2310538abf23324fa66539ccd5439cdbc4760830
RIchard Farrell <richardmfarrell@gmail.com>
2019-07-04 18:29:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string.toLowerCase()){
-    return = "I can\'t hear you!";
+    return = "no";
   }
   else if (string.toUpperCase()){
     return = "YES INDEED!";
